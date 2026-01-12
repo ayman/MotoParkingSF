feb5b6ce1e28cf6546c7a35cd541d27483502285
David A. Shamma <1846+ayman@users.noreply.github.com>
2026-01-12 14:12:14 -0800

    Adding metered spots.

--- MotoParkSF/Data/ParkingSpot.swift
+++ MotoParkSF/Data/ParkingSpot.swift
@@ -4,6 +4,8 @@
 //
 //  Created by David A. Shamma on 1/12/26.
 //
+// Metered: https://data.sfgov.org/api/views/uf55-k7py/rows.json
+// Unmetered: https://data.sfgov.org/api/views/egmb-2zhs/rows.json
 
 import Foundation
 import MapKit
